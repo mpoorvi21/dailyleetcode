8d0b5a40f7caa7cc8621304433236860322457b8
mpoorvi21 <poorvimah21@gmail.com>
2025-06-02 08:01:10 +0530

    added two sum

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+public class TwoSum {
+
+    public static void main(String args[]){
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(new int[]{10, 20, 30, 40, 50},90);
+        System.out.println(result);
+    }
+    public int[] twoSum(int[] nums, int target){
+
+        int ans[] = new int[2];
+        for(int i =0;i< nums.length;i++){
+            for(int j =i+1;j< nums.length;j++){
+                if(nums[i] + nums[j] == target){
+
+                    ans[0]=i;
+                    ans[1]=j;
+                    System.out.print(ans);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
